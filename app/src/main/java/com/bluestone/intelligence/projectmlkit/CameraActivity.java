823905edf6aa65e5b8ed40ae7ac54023f29ac7c3
N.Sanjeev kumar <sanjeevitcit@gmail.com>
2018-06-24 18:37:43 +0530

    Firebase image: Adding rotation compensation
    * Adding rotation compensation to the live image captured
    * Increasing minimum SDK version to 21 due to camera manager
      dependencies
    
    Signed-off-by: N.Sanjeev kumar <sanjeevitcit@gmail.com>

--- app/src/main/java/com/bluestone/intelligence/projectmlkit/CameraActivity.java
+++ app/src/main/java/com/bluestone/intelligence/projectmlkit/CameraActivity.java
@@ -1,12 +1,18 @@
 package com.bluestone.intelligence.projectmlkit;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -32,6 +38,7 @@ public class CameraActivity extends AppCompatActivity{
     private Button captureButton;
 
     private static Context context;
+    private String backCameraId = "1";
 
     private Camera camera;
     private CameraSurfaceView cameraSurfaceView;
@@ -40,6 +47,14 @@ public class CameraActivity extends AppCompatActivity{
     private FirebaseVisionImage image;
     private FirebaseVisionImageMetadata metadata;
     private FirebaseVisionLabelDetector detector;
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +69,18 @@ public class CameraActivity extends AppCompatActivity{
         options = new FirebaseVisionLabelDetectorOptions.Builder()
                         .setConfidenceThreshold(0.8f).build();
 
+        int rotation = 0;
+        try {
+            rotation = getRotationCompensation(backCameraId, this, this);
+        }
+        catch (CameraAccessException e){
+        }
+
         metadata = new FirebaseVisionImageMetadata.Builder()
                 .setWidth(1280)
                 .setHeight(720)
                 .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
-                .setRotation(0)
+                .setRotation(rotation)
                 .build();
 
         detector = FirebaseVision.getInstance().getVisionLabelDetector();
@@ -146,5 +168,41 @@ public class CameraActivity extends AppCompatActivity{
                                     public void onFailure(@NonNull Exception e) {
                                     }
                                 });
+    }
+
+    private int getRotationCompensation(String cameraId, Activity activity, Context context)
+            throws CameraAccessException
+    {
+        int deviceRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int rotationCompensation = ORIENTATIONS.get(deviceRotation);
+
+        CameraManager cameraManager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
+
+        int sensorOrientation = 0;
+        if (cameraManager != null)
+            sensorOrientation = cameraManager.getCameraCharacteristics(cameraId)
+                .get(CameraCharacteristics.SENSOR_ORIENTATION);
+
+        rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360;
+
+        int result;
+        switch (rotationCompensation) {
+            case 0:
+                result = FirebaseVisionImageMetadata.ROTATION_0;
+                break;
+            case 90:
+                result = FirebaseVisionImageMetadata.ROTATION_90;
+                break;
+            case 180:
+                result = FirebaseVisionImageMetadata.ROTATION_180;
+                break;
+            case 270:
+                result = FirebaseVisionImageMetadata.ROTATION_270;
+                break;
+            default:
+                result = FirebaseVisionImageMetadata.ROTATION_0;
+                Log.e("Rotation_tag", "Bad rotation value: " + rotationCompensation);
+        }
+        return result;
     }
 }
